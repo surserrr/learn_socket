4d6b4fdf9876332cf5b8bd1912a64fffb45357f7
Surser <724598540@qq.com>
2020-02-21 21:49:28 +0800

    2.15 将报头和报体封装

--- EasyTCPClient/EasyTCPClient/client.cpp
+++ EasyTCPClient/EasyTCPClient/client.cpp
@@ -26,7 +26,9 @@ using namespace std;
 enum CMD
 {
     CMD_LOGIN,
+    CMD_LOGIN_RESULT,
     CMD_LOGOUT,
+    CMD_LOGOUT_RESULT,
     CMD_ERROR
 };
 
@@ -36,22 +38,46 @@ struct DataHeader
     short cmd;
 };
 //DataPacket
-struct Login
+struct Login:public DataHeader
 {
+    Login()
+    {
+        dataLength = sizeof(Login);
+        cmd = CMD_LOGIN;
+    }
     char userName[32];
     char PassWord[32];
 };
-struct LoginResult
+struct LoginResult:public DataHeader
 {
+    LoginResult()
+    {
+        dataLength = sizeof(LoginResult);
+        cmd = CMD_LOGIN_RESULT;
+        result = 0;
+    }
     int result;
 };
-struct Logout
+struct Logout:public DataHeader
 {
+    Logout()
+    {
+        dataLength = sizeof(Logout);
+        cmd = CMD_LOGOUT;
+    }
     char userName[32];
 };
-struct LogoutResult{
+struct LogoutResult:public DataHeader
+{
+    LogoutResult()
+    {
+        dataLength = sizeof(LogoutResult);
+        cmd = CMD_LOGOUT_RESULT;
+        result = 0;
+    }
     int result;
 };
+
 
 int main(int argc, const char * argv[]) {
 //    1.建立一个socket
@@ -88,28 +114,23 @@ int main(int argc, const char * argv[]) {
         }
         else if( 0 == strcmp(cmdBuf, "login"))
         {
-            Login login = {"fjt","fjtmm"};
-            DataHeader dh = {sizeof(login), CMD_LOGIN};
+            Login login;
+            strcpy(login.userName, "fjt");
+            strcpy(login.PassWord, "fjtmm");
             //5.向服务器发送请求命令
-            send(_sock, (const char*)&dh, sizeof(dh), 0);
             send(_sock, (const char*)&login, sizeof(login), 0);
             // 接受服务器返回的数据
-            DataHeader retHeader = {};
             LoginResult loginRet = {};
-            recv(_sock, (char*) &retHeader, sizeof(retHeader), 0);
             recv(_sock, (char*) &loginRet, sizeof(loginRet), 0);
             cout<<"LoginResult:"<<loginRet.result;
         }
         else if( 0 == strcmp(cmdBuf, "logout"))
         {
-            Logout logout = {"fjt"};
-            DataHeader dh = {sizeof(logout), CMD_LOGOUT};
-            send(_sock, (const char*)&dh, sizeof(dh), 0);
+            Logout logout;
+            strcpy(logout.userName, "fjt");
             send(_sock, (const char*)&logout, sizeof(logout), 0);
             // 接受服务器返回的数据
-            DataHeader retHeader = {};
             LoginResult logoutRet = {};
-            recv(_sock, (char*) &retHeader, sizeof(retHeader), 0);
             recv(_sock, (char*) &logoutRet, sizeof(logoutRet), 0);
             cout<<"LogoutRet:"<<logoutRet.result;
         }
