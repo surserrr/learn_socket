4fed70e095ca991f89355bcc2202ea723a500b0e
Surser <724598540@qq.com>
2020-02-24 15:02:15 +0800

    2-25客户端添加输入线程，升级为跨平台

--- EasyTCPClient/EasyTCPClient/client.cpp
+++ EasyTCPClient/EasyTCPClient/client.cpp
@@ -95,7 +95,7 @@ int processor (SOCKET _cSock)
     //缓冲区
     char szRecv[4096] = {};
     //5. 接受客户端请求
-    int nLen = recv(_cSock, (char *)&szRecv, sizeof(DataHeader), 0);
+    int nLen = (int)recv(_cSock, (char *)&szRecv, sizeof(DataHeader), 0);
     DataHeader* header = (DataHeader*)szRecv;
     if(nLen<=0){
         cout<<"与服务器断开连接，任务结束"<<endl;
@@ -107,7 +107,6 @@ int processor (SOCKET _cSock)
             recv(_cSock, (char *)&szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
             LoginResult* login = (LoginResult*) szRecv;
             cout<<"收到服务器消息：CMD_LOGIN_RESULT,数据长度："<<login->dataLength<<endl;
-                
         }
         break;
         case CMD_LOGOUT_RESULT:
@@ -124,11 +123,53 @@ int processor (SOCKET _cSock)
             cout<<"收到服务器消息：CMD_NEW_USER_JOIN,数据长度："<<userJoin->dataLength<<endl;
         }
         break;
+        default:
+            cout<<"test"<<endl;
+        break;
     }
-    return 1;
+    return 0;
+}
+bool g_bRun = true;
+void cmdThread(SOCKET sock)
+{
+    while(g_bRun)
+    {
+        char cmdBuf[256] = {};
+        cin>>cmdBuf;
+        if(0==strcmp(cmdBuf, "exit"))
+        {
+            g_bRun = false;
+            cout<<"退出线程"<<endl;
+            break;
+        }
+        else if (0 ==strcmp(cmdBuf, "login"))
+        {
+            Login login;
+            strcpy(login.userName, "fjt");
+            strcpy(login.PassWord,"fjtmm");
+            send(sock, (const char*)&login, sizeof(Login), 0);
+        }
+        else if (0 ==strcmp(cmdBuf, "logout"))
+        {
+            Logout logout;
+            strcpy(logout.userName, "fjt");
+            send(sock, (const char*)&logout, sizeof(Logout), 0);
+        }
+        else
+        {
+            cout<<"不支持的命令"<<endl;
+        }
+    }
 }
 
 int main(int argc, const char * argv[]) {
+#ifdef _WIN32
+    // 启动windows socket2.x 环境
+    WORD vwe = MAKEWORD(2,2);
+    WSADATA dat;
+    WSAStartup(ver,&dat);
+#endif
+    
 //    1.建立一个socket
     SOCKET _sock = socket(AF_INET, SOCK_STREAM, 0);
     if(INVALID_SOCKET == _sock){
@@ -141,7 +182,11 @@ int main(int argc, const char * argv[]) {
     sockaddr_in _sin = {};
     _sin.sin_family = AF_INET;
     _sin.sin_port = htons(4567);
+#ifdef _WIN32
+    _sin.sin_addr.S_un.s_addr = inet_addr("127.0.0.1");
+#else
     _sin.sin_addr.s_addr = inet_addr("127.0.0.1");
+#endif
     int ret = connect(_sock, (sockaddr *) &_sin, sizeof(sockaddr_in));
     if(SOCKET_ERROR == ret){
         cout<<"错误！连接服务器失败\n";
@@ -149,14 +194,17 @@ int main(int argc, const char * argv[]) {
     else{
         cout<<"连接服务器成功\n";
     }
+    //启动线程
+    std::thread t1(cmdThread,_sock);
+    t1.detach();
     
-    while (true)
+    while (g_bRun)
     {
         fd_set fdReads;
         FD_ZERO(&fdReads);
         FD_SET(_sock,&fdReads);
-        timeval t={0,0};
-        int ret = select(_sock, &fdReads, NULL, NULL, &t);
+        timeval t={1,0};
+        int ret = select(_sock+1, &fdReads, NULL, NULL, &t);
         if(ret<0){
             cout<<"select任务结束1"<<endl;
             break;
@@ -169,16 +217,16 @@ int main(int argc, const char * argv[]) {
                 break;
             }
         }
-        cout<<"空闲时间处理其他业务 .."<<endl;
-        Login login;
-        strcpy(login.userName, "fjt");
-        strcpy(login.PassWord,"fjtmm");
-        send(_sock, (const char*)&login, sizeof(Login), 0);
+        //cout<<"空闲时间处理其他业务 .."<<endl;
         //sleep(1000);
     }
 //    7.关闭套接字close
+#ifdef _WIN32
+    closesocket(_sock);
+#else
     close(_sock);
+#endif
     cout<<"客户端已退出。\n";
-    getchar();
+ //   getchar();
     return 0;
 }
--- EasyTCPServer/EasyTCPServer/server.cpp
+++ EasyTCPServer/EasyTCPServer/server.cpp
@@ -3,8 +3,10 @@
 //  Copyright © 2020 Surser. All rights reserved.
 //
 #ifdef _WIN32
+    #define WIN32_LEAN_AND_MEAN
     #include<windows.h>
     #include<WinSock2.h>
+    #pragma comment(lib,"ws2_32.lib")
 #else
     #include <unistd.h>
     #include<arpa/inet.h>
@@ -29,7 +31,7 @@ enum CMD
     CMD_LOGOUT,
     CMD_LOGOUT_RESULT,
     CMD_NEW_USER_JOIN,
-    CMD_ERRO
+    CMD_ERROR
 };
 
 struct DataHeader
@@ -95,7 +97,7 @@ int processor (SOCKET _cSock)
     //缓冲区
     char szRecv[4096] = {};
     //5. 接受客户端请求
-    int nLen = recv(_cSock, (char *)&szRecv, sizeof(DataHeader), 0);
+    int nLen = (int)recv(_cSock, (char *)&szRecv, sizeof(DataHeader), 0);
     DataHeader* header = (DataHeader*)szRecv;
     if(nLen<=0){
         cout<<"客户端<Socket="<<_cSock<<">已退出，任务结束。\n";
@@ -108,8 +110,11 @@ int processor (SOCKET _cSock)
             Login* login = (Login*) szRecv;
         cout<<"收到客户端<Socket="<<_cSock<<">请求：CMD_LOGIN,数据长度："<<login->dataLength<<",userName="<<login->userName<<",PassWord="<<login->PassWord<<endl;
             //忽略判断用户密码是否正确的过程
-            LoginResult ret;
-            send(_cSock, (char*)&ret, sizeof(ret), 0);
+            LoginResult ret = {};
+            send(_cSock, (char*)&ret, sizeof(LoginResult), 0);
+            //char msgBuf[128]= "Xiao Fang.";
+            // 8. send 向客户端发送一段数据
+            //send(_cSock, msgBuf, strlen(msgBuf)+1, 0);
         }
         break;
         case CMD_LOGOUT:
@@ -119,7 +124,7 @@ int processor (SOCKET _cSock)
             cout<<"收到客户端<Socket="<<_cSock<<">请求：CMD_LOGOUT,数据长度："<<logout->dataLength<<endl;
             //忽略判断用户密码是否正确的过程
             LogoutResult ret = {};
-            send(_cSock, (char*)&ret, sizeof(ret), 0);
+            send(_cSock, (char*)&ret, sizeof(LogoutResult), 0);
         }
         break;
         default:
@@ -127,18 +132,28 @@ int processor (SOCKET _cSock)
             send(_cSock, (char*)&header, sizeof(header), 0);
         break;
     }
-    return 1;
+    return 0;
 }
 
 int main(int argc, const char * argv[]) {
-//
+#ifdef _WIN32
+    // 启动windows socket2.x 环境
+    WORD vwe = MAKEWORD(2,2);
+    WSADATA dat;
+    WSAStartup(ver,&dat);
+#endif
     //1. 建立一个socket 三个参数变量，1 ip编码方式 ipv4；2 流式，面向字节流的；3 TCP；
     SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     //2. bind 绑定用于接受客户端连接的端口
     sockaddr_in _sin = {};
     _sin.sin_family = AF_INET;
     _sin.sin_port = htons(4567); //网络和本机字节序编码不同，需要转换，一个是大端编码，一个是小端编码
-    _sin.sin_addr.s_addr = INADDR_ANY; // 本机ip地址
+    #ifdef _WIN32
+        _sin.sin_addr.S_un.s_addr = INADDR_ANY;
+    #else
+        _sin.sin_addr.s_addr = INADDR_ANY;
+    #endif
+    //_sin.sin_addr.s_addr = INADDR_ANY; // 本机ip地址
     if(SOCKET_ERROR == ::bind(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in))){
         cout<<"ERROR!绑定用于接受客户端连接的端口失败！\n";
     }
@@ -153,7 +168,7 @@ int main(int argc, const char * argv[]) {
     else{
         cout<<"监听网络端口成功！\n";
     }
-    
+    cout<<"本进程socket="<<_sock<<endl;
     while(true)
     {
         //伯克利套接字 BSD socket 描述符
@@ -168,15 +183,18 @@ int main(int argc, const char * argv[]) {
         FD_SET(_sock,&fdRead);
         FD_SET(_sock,&fdWrite);
         FD_SET(_sock,&fdExp);
-        
+        SOCKET maxSock = _sock;
         for (int n=(int)g_clients.size()-1; n>=0; n--)
         {
             FD_SET (g_clients[n],&fdRead);
+            if(maxSock < g_clients[n]){
+                maxSock = g_clients[n];
+            }
         }
         ///第一个参数nfds是一个整数，指fd_set集合中所有socket的范围，而不是数量。
         ///即是所有描述符最大值+1，在windows中可以写0.
-        timeval t={0,0};
-        int ret = select(_sock+1, &fdRead, &fdWrite, &fdExp, &t); //最后一个参数为NULL，会一直阻塞到有信息进来;若设置一段时间，则会阻塞一段时间。服务器既可以处理多个客户端的请求，也可以在空闲时候做服务器自己的业务处理（例如主动给客户端发信息）。
+        timeval t={1,0};
+        int ret = select(maxSock+1, &fdRead, &fdWrite, &fdExp, &t); //最后一个参数为NULL，会一直阻塞到有信息进来;若设置一段时间，则会阻塞一段时间。服务器既可以处理多个客户端的请求，也可以在空闲时候做服务器自己的业务处理（例如主动给客户端发信息）。
         if(ret<0)
         {
             cout<<"select任务结束."<<endl;
@@ -190,7 +208,11 @@ int main(int argc, const char * argv[]) {
             sockaddr_in clientAddr = {};
             socklen_t nAddrLen = sizeof(sockaddr_in);
             SOCKET _cSock = INVALID_SOCKET;
+#ifdef _WIN32
             _cSock = accept(_sock, (sockaddr*)&clientAddr, &nAddrLen);
+#else
+            _cSock = accept(_sock, (sockaddr*)&clientAddr, (socklen_t*)&nAddrLen);
+#endif
             if(INVALID_SOCKET == _cSock){
                 cout<<"ERROR!接收到无效客户端SOCKET！\n";
             }
@@ -206,27 +228,40 @@ int main(int argc, const char * argv[]) {
             }
            
         }
-        for (size_t n=0; n < sizeof(fdRead.fds_bits); n++)
+        
+        for (int n=(int)g_clients.size()-1; n>=0; n--)
         {
-            if(-1 == processor(fdRead.fd_array[n]))
+            if(FD_ISSET(g_clients[n], &fdRead))
             {
-                auto iter = find(g_clients.begin(),g_clients.end(),fdRead.fd_array[n]);
-                if (iter != g_clients.end()){
-                    g_clients.erase(iter);
+                if(-1 == processor(g_clients[n]))
+                {
+                    auto iter = g_clients.begin() + n;
+                    if(iter != g_clients.end())
+                    {
+                        g_clients.erase(iter);
+                    }
                 }
             }
         }
-        cout<<"空闲时间处理其他业务 .."<<endl;
-        
+
+        //cout<<"空闲时间处理其他业务 .."<<endl;
     }
     
-    
-    for(size_t n = g_clients.size()-1;n >=0;n--)
+#ifdef _WIN32
+    for(int n = g_clients.size()-1;n >=0;n--)
+    {
+        closesocket(g_clients[n]);
+    }
+    //8. 关闭套接字closesocket
+    closesocket(_sock);
+#else
+    for(int n = g_clients.size()-1;n >=0;n--)
     {
         close(g_clients[n]);
     }
     //8. 关闭套接字closesocket
     close(_sock);
+#endif
 //
     cout<<"服务器已退出。\n";
     getchar();
